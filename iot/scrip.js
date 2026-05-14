fece77df827390a6c00c50a89c99b232847c228e
KevProgram85 <kevindiegogutierrez28@gmail.com>
2026-05-13 21:44:28 -0500

    Integrate Firebase for LED control functions

--- iot/scrip.js
+++ iot/scrip.js
@@ -1,8 +1,38 @@
-function encender() {
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getDatabase,
+  ref,
+  set
+} from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+
+  apiKey: "TU_API_KEY",
+
+  authDomain: "TU_AUTH_DOMAIN",
+
+  databaseURL: "TU_DATABASE_URL",
+
+  projectId: "TU_PROJECT_ID",
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
+window.encender = function () {
+
+  set(ref(db, 'led'), "ON");
+
   const estado = document.getElementById("estado");
   const led = document.getElementById("led");
 
   estado.innerHTML = "● ENCENDIDO";
+
   estado.classList.remove("apagado");
   estado.classList.add("encendido");
 
@@ -13,11 +43,15 @@ function encender() {
     "Última acción: Encendido - " + new Date().toLocaleTimeString();
 }
 
-function apagar() {
+window.apagar = function () {
+
+  set(ref(db, 'led'), "OFF");
+
   const estado = document.getElementById("estado");
   const led = document.getElementById("led");
 
   estado.innerHTML = "● APAGADO";
+
   estado.classList.remove("encendido");
   estado.classList.add("apagado");
 
